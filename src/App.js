66ccf74e1024a82c69e50c8f624ae97d0904abb4
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-10 01:26:51 -0700

    Updated App.js and index.js to base frontend site + deleted srcfiles that were not needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/ML/');
+    setSongs(response.data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3> Hello WOrld</h3>
     </div>
   );
 }
 
 export default App;
+
+
